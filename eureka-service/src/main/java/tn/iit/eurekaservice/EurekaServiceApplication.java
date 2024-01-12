588fa484c09a82649ba2df79fac8585230cb0945
Mohamed Tijani Daoud <mohamed.daoud99@hotmail.com>
2024-01-12 09:30:58 +0100

    fix eureka

--- eureka-service/src/main/java/tn/iit/eurekaservice/EurekaServiceApplication.java
+++ eureka-service/src/main/java/tn/iit/eurekaservice/EurekaServiceApplication.java
@@ -2,11 +2,8 @@ package tn.iit.eurekaservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer
-
 public class EurekaServiceApplication {
 
     public static void main(String[] args) {
